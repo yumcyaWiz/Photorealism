df61d3f608ecb43d57985c77373cd3c8071a6fb9
yumcyawiz <sergeyinverse@gmail.com>
2018-08-23 21:54:38 +0900

    add primitive

--- hit.h
+++ hit.h
@@ -1,11 +1,15 @@
 #ifndef HIT_H
 #define HIT_H
 #include "vec3.h"
+
+class Primitive;
+
 class Hit {
   public:
     double t;
     Vec3 hitPos;
     Vec3 hitNormal;
+    Primitive* hitPrimitive;
 
     Hit() {};
 };
---
+++ primitive.h
@@ -0,0 +1,23 @@
+#ifndef PRIMITIVE_H
+#define PRIMITIVE_H
+#include <memory>
+#include "shape.h"
+#include "material.h"
+class Primitive {
+  public:
+    std::shared_ptr<Shape> shape;
+    std::shared_ptr<Material> material;
+
+    Primitive(const std::shared_ptr<Shape>& _shape, const std::shared_ptr<Material>& _material) : shape(_shape), material(_material) {};
+
+    bool intersect(const Ray& ray, Hit& res) const {
+      if(shape->intersect(ray, res)) {
+        res.hitPrimitive = this;
+        return true;
+      }
+      else {
+        return false;
+      }
+    };
+};
+#endif
