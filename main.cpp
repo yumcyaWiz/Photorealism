d0f77a59371a6ed2a98ca91456c4365037ea4f4c
yumcyawiz <sergeyinverse@gmail.com>
2018-08-28 23:30:07 +0900

    add submodule cpptoml

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@
 
 
 int main() {
-  int N = 10;
+  int N = 300;
   auto film = std::make_shared<Film>(512, 512);
   auto camera = std::make_shared<ThinLensCamera>(Vec3(0, 1, -5), Vec3(0, 0, 1), film, Vec3(0, 0.5, 2.5), 1.3, 8.0);
 
@@ -42,6 +42,7 @@ int main() {
   auto blue = std::make_shared<Lambert>(RGB(0.2, 0.2, 0.8));
   auto green = std::make_shared<Lambert>(RGB(0.2, 0.8, 0.2));
   auto red = std::make_shared<Lambert>(RGB(0.8, 0.2, 0.2));
+  auto mirror = std::make_shared<Mirror>(RGB(0.9));
   auto phong = std::make_shared<Phong>(RGB(0.9), 0.0, 100.0);
 
   std::vector<std::shared_ptr<Light>> lights;
@@ -51,7 +52,7 @@ int main() {
   //lights.push_back(light2);
 
   std::vector<std::shared_ptr<Primitive>> prims;
-  auto prim1 = std::make_shared<Primitive>(floor, phong);
+  auto prim1 = std::make_shared<Primitive>(floor, mirror);
   auto prim2 = std::make_shared<Primitive>(right_wall, red);
   auto prim3 = std::make_shared<Primitive>(left_wall, green);
   auto prim4 = std::make_shared<Primitive>(back_wall, white);
