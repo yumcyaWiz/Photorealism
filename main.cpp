96047be25191ed87203fbd3b6001bd310735d1ef
yumcyawiz <hashdolf1923@gmail.com>
2019-09-04 19:28:55 +0900

    rtcamp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,11 @@
 #include "toml_loader.h"
 
 int main(int argc, char** argv) {
-  std::string toml_filepath;
-  int opt;
-  while((opt = getopt(argc, argv, "i:on")) != -1) {
-    switch(opt) {
-      case 'i':
-        toml_filepath = optarg;
-        break;
-    }
-  }
-
   std::shared_ptr<Scene> scene;
   std::shared_ptr<Integrator> integrator;
 
   TomlLoader loader;
-  loader.load(toml_filepath, scene, integrator);
+  loader.load("rtcamp.toml", scene, integrator);
 
   integrator->render_rtcamp(*scene);
   return 0;
